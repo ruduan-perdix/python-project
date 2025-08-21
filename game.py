47da9dbb1945bb6c4ddf852d953378011d83e6e9
ruduan-perdix <ruduan@perdix.co.za>
2025-08-21 22:33:29 +0200

    code progress

--- game.py
+++ game.py
@@ -5,5 +5,16 @@ Robot Challenge
 """
 x = "Ruduan"
 
-print(x)
+print("Robot Game Simulator")
+print("--------------------\n")
+
+# Robot must have 4 hitpoints
+hitpoints = 4
+
+# Robot will start in the corner of a grid
+position = [0, 0]
+
+# The grid will be a 4 x 4 grid
+# Enter Random obstacles
+obstacles = [(1, 1), (2, 2), (2, 3), (4,1)]
 
