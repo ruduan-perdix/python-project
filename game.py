1b574e27b47f1ea2771a648879936eaff96be787
ruduan-perdix <ruduan@perdix.co.za>
2025-08-22 11:41:43 +0200

    Added Obstacle Detection and improved gameplay.

--- game.py
+++ game.py
@@ -3,7 +3,8 @@ Python-Project
 2025-08-21
 Robot Challenge
 """
-
+# Game name
+print("--------------------")
 print("Robot Game Simulator")
 print("--------------------\n")
 
@@ -11,7 +12,8 @@ print("--------------------\n")
 name = input("ENTER a name for the robot: ")
 
 # Print the input
-print(f"Your ROBOT's name is: {name}\n")
+print("***************************************************************************")
+print(f"Your ROBOT's name is: {name}")
 
 # Robot must have 4 hitpoints
 hitpoints = 4
@@ -20,12 +22,13 @@ hitpoints = 4
 position = [0, 0]
 
 # The grid will be a 4 x 4 grid
-# Enter Random obstacles
+# Enter obstacles coordinates
 obstacles = [(1, 1), (2, 2), (2, 3), (4,1)]
 
-print("***************************************************************************\n")
-print(f"Welcome Player! The rules are simple: Don't let {name} hit any obstacles!\n")
-print(f"Take note of the following commands to navigate {name} through the 4x4 grid:")
+print("***************************************************************************")
+print(f"Welcome! The rules are simple: Don't let {name} hit any obstacles!")
+print("***************************************************************************")
+print(f"\nTake note of the following commands to navigate {name} through the 4x4 grid:\n")
 print("1: u = up")
 print("2: d = down")
 print("3: l = left")
@@ -37,31 +40,37 @@ print("*************************************************************************
 while hitpoints > 0:
     print(f"{name}'s coordinates on the grid are: {position}")
     print(f"{name} has a total of {hitpoints} hitpoints left.\n")
+    print("***************************************************************************")
 
 # Prompt for the command
-    command = input("\n----------> Enter a command (u / d / l / r): ")
+    command = input(f"----> Give {name} the next command (u / d / l / r): ")
 
 # Implement commands and movement for the Robot
     if command == "l":
-            print("\n\nYou entered 'l' to move left.\n")
+            print("***************************************************************************")
+            print(f"\nYou entered 'l' to move {name} one space left.\n")
             position[0] -= 1
     elif command == "r":
-            print("\n\nYou entered 'r' to move right.\n")
+            print("***************************************************************************")
+            print(f"\nYou entered 'r' to move {name} one space right.\n")
             position[0] += 1
     elif command == "u":
-            print("\n\nYou entered 'u' to move up.\n")
+            print("***************************************************************************")
+            print(f"\nYou entered 'u' to move {name} one space up.\n")
             position[1] += 1
     elif command == "d":
-            print("\n\nYou entered 'd' to move down.\n")
+            print("***************************************************************************")
+            print(f"\nYou entered 'd' to move {name} one space down.\n")
             position[1] -= 1
     else:
-            print(f"\n\nIncorrect input. You can only use 'u', 'd', 'l' and 'r' to move {name}\n")
+            print("***************************************************************************")
+            print(f"\nIncorrect input. You can only use 'u', 'd', 'l' and 'r' to move {name}\n")
 
 # CHECK Boundary Box
     if position [0] < 0 or position [0] > 4 or position[1] < 0 or position[1] > 4:
-            print("*****************************************************")
+            print("***************************************************************************")
             print("Out of Bounds! Please stay within the 4 x 4 grid.")
-            print("*****************************************************\n")
+            print("***************************************************************************\n")
 
 # REVERSE movement for the Robot
             if command == "l":
@@ -73,7 +82,24 @@ while hitpoints > 0:
             elif command == "d":
                 position[1] += 1
 
+# Obstacle detection                
+    if (position [0], position [1]) in obstacles:
+        hitpoints -= 1
+        print(f"OH NO! WATCH OUT!\n{name} hit an obstacle and lost 1 hitpoint.\n")
+        print(f"{name} should try a different route instead.\n")
+# REVERSE movement for the Robot
+        if command == "l":
+            position[0] += 1
+        elif command == "r":
+            position[0] -= 1
+        elif command == "u":
+            position[1] -= 1
+        elif command == "d":
+            position[1] += 1
 
+        if hitpoints == 0:
+            print(f"Oh dear! {name} died!")
+            break
 
-
-
+# End of game
+print("\nGoodbye.\n\n\n")
